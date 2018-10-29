8e1d70d68d292765cef2ab50ab094b53b788eabe
Natallia Savelyeva <natallia.savelyeva@gmail.com>
2018-10-29 23:32:23 +0100

    Removed unnecessary annotation MappedSuperClass

--- database/src/main/java/com/savelyeva/model/BaseEntity.java
+++ database/src/main/java/com/savelyeva/model/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.savelyeva.model;
 
-import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@MappedSuperclass
 public interface BaseEntity<PK extends Serializable> {
 
     PK getId();
